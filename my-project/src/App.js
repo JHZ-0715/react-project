39eb766e773a6b3c89aaaf2106b97f0864f9cb16
JHZ-0715 <1124426612@qq.com>
2017-11-28 10:24:13 +0800

    'ads'

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,20 +1,40 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+  constructor(){
+    super();
+    this.state = {
+      films: []
+    }
+  }
+  componentDidMount(){
+    var that = this;
+    axios.get("/v4/api/film/now-playing?__t=1511310189846&page=1&count=5")
+    .then(function(res){
+      console.log(res);
+      that.setState({
+        films:res.data.data.films
+      })
+    })
+  }
+  render(){
+    var list = this.state.films.map((item,index)=>{
+
+    return(
+      <div>
+        <img src = {item.cover.origin} />
+        <h2>{item.name}</h2>
       </div>
-    );
+      );
+    })
+    return(
+      <div>
+        {list}
+      </div>
+      )
   }
 }
 
