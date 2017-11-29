a155d8f2b7b250de834197a6d2b70a9cc80511b9
gaoyue0525 <619215426@qq.com>
2017-11-29 16:49:08 +0800

    'change'

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -29,34 +29,38 @@ class App extends Component {
 
 
     return(
-
-
           <Router>
             <div>
-
-
-
               <ul className="footer">
-                <li><NavLink activeClassName="active" to="/home">精选</NavLink></li>
-                <li><NavLink activeClassName="active" to="/Show">演出</NavLink></li>
-                <li><NavLink activeClassName="active" to="/login">我的</NavLink></li>
+                <li>
+                  <NavLink activeClassName="active" to="/home">
+                    <span className="icon1"></span>
+                    <p className="active">精选</p>
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink activeClassName="active" to="/Show">
+                    <span className="icon2"></span>
+                    <p>演出</p>
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink activeClassName="active" to="/login">
+                    <span className="icon3"></span>
+                    <p>我的</p>
+                  </NavLink>
+                </li>
               </ul>
-
               <Switch>
                 <Redirect exact from="/" to="/home" />
                 <Route path="/home" component={Home}/>
                 <Route path="/Login" component={Login}/>
                 <Route path="/Show" component={Show}/>
                 <Route path="/Regist" component={Regist}/>
-                
               </Switch>
             </div>
           </Router>
-
-
       )
-
-
   }
 }
 
